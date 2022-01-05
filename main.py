06e9ee43ef11383a6de262d573c75ee256a0fb57
theAfish <85424334+theAfish@users.noreply.github.com>
2022-01-05 18:47:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ ti.init()
 
 v_res = 500
 
+RED = [1.0, 0.0, 0.0]
+GREEN = [0.0, 1.0, 0.0]
+BLUE = [0.0, 0.0, 1.0]
+CYAN_GREEN = [0.0, 1.0, 0.5]
+
 @ti.data_oriented
 class Rasterizer:
     def __init__(self):
-        self.triangles = []
+        self.triangles = None
         self.camera = Camera()
         self.res = self.camera.res
         self.M_mvp = ti.Matrix.field(4, 4, dtype=ti.f32, shape=())
@@ -27,35 +32,54 @@ class Rasterizer:
     def get_mvp_mat(self):
         self.M_mvp[None] = self.camera.get_vp_mat() @ self.camera.get_pers_mat() @ self.camera.get_cam_mat()
 
+    def draw_line(self, begin, end):
+        line_color = [1., 1., 1.]
+        x1 = begin.x
+        y1 = begin.y
+        x2 = end.x
+        y2 = end.y
+        xe, ye, x, y = 0, 0, 0, 0
+        dx = int(x2 - x1)
+        dy = int(y2 - y1)
+        dx1 = ti.abs(dx)
+        dy1 = ti.abs(dy)
+        px = 2 * dy1 - dx1
+        py = 2 * dx1 - dy1
+        if dy1 <= dx1:
+            if dx >= 0:
+                x = x1
+                y = y1
+                xe = x2
+            else:
+                x = x2
+                y = y2
+                xe = x1
+
     @ti.kernel
-    def draw_triangle(self, triangle: ti.template()):
-        pa = ti.Vector([triangle.vertex[0].x, triangle.vertex[0].y])
-        pb = ti.Vector([triangle.vertex[1].x, triangle.vertex[1].y])
-        pc = ti.Vector([triangle.vertex[2].x, triangle.vertex[2].y])
-        min_pos = ti.cast(ti.min(ti.min(pa, pb), pc) - 0.5, ti.i32)
-        min_pos = ti.max(min_pos, ti.Vector([0, 0]))
-        max_pos = ti.cast(ti.max(ti.max(pa, pb), pc) + 0.5, ti.i32)
-        max_pos = ti.min(max_pos, self.res)
-        for x, y in ti.ndrange((min_pos.x, max_pos.x), (min_pos.y, max_pos.y)):
-            if triangle.inside(ti.Vector([x + 0.5, y + 0.5])):
-                self.image[x, y] = triangle.color[None]
+    def draw_triangles(self):
+        for t in range(self.triangles.triangle_num[None]):
+            p1 = self.M_mvp[None] @ self.triangles.A[t]
+            p2 = self.M_mvp[None] @ self.triangles.B[t]
+            p3 = self.M_mvp[None] @ self.triangles.C[t]
+            pa = ti.Vector([p1.x, p1.y])
+            pb = ti.Vector([p2.x, p2.y])
+            pc = ti.Vector([p3.x, p3.y])
+            min_pos = ti.cast(ti.min(ti.min(pa, pb), pc) - 0.5, ti.i32)
+            min_pos = ti.max(min_pos, ti.Vector([0, 0]))
+            max_pos = ti.cast(ti.max(ti.max(pa, pb), pc) + 0.5, ti.i32)
+            max_pos = ti.min(max_pos, self.res)
+            for x, y in ti.ndrange((min_pos.x, max_pos.x), (min_pos.y, max_pos.y)):
+                if self.triangles.inside(ti.Vector([x + 0.5, y + 0.5]), pa, pb, pc):
+                    self.image[x, y] = self.triangles.color[t]
 
-    def draw(self):
-        for i in range(len(self.triangles)):
-            new_triangle = Triangle(color=self.triangles[i].color[None])
-            for v in range(3):
-                p = self.M_mvp[None] @ self.triangles[i].vertex[v]
-                new_triangle.set_vertex(v, p)
-            self.draw_triangle(new_triangle)
-
-    def add_triangle(self, triangle):
-        self.triangles.append(triangle)
+    def set_triangles(self, triangles):
+        self.triangles = triangles
 
     def show(self):
         t = 0
         while self.window.running:
             self.image.fill(0)
-            self.draw()
+            self.draw_triangles()
             self.canvas.set_image(self.image)
             self.window.show()
             self.camera.debug_move(t)
@@ -78,11 +102,12 @@ class Camera:
         self.reset()
 
     def reset(self):
-        self.look_from[None] = [2.0, 0.0, 0.0]
-        self.look_at[None] = [0.0, 0.0, 2.0]
+        self.look_from[None] = [5.0, 5.0, 1.0]
+        self.look_at[None] = [0.0, 0.0, 0.0]
 
     def debug_move(self, t):
-        self.look_at[None] = [0.0, 0.0, ti.sin(t * 0.1) * 2]
+        # self.look_at[None] = [0.0, 0.0, ti.sin(t * 0.01) * 2]
+        self.look_from[None] = [5.0*ti.cos(t * 0.01), 5.0, 5.0*ti.sin(t * 0.01)]
 
     @ti.func
     def get_cam_mat(self):
@@ -127,23 +152,37 @@ class Camera:
 
 
 @ti.data_oriented
-class Triangle:
-    def __init__(self, color=None):
+class Triangles:
+    def __init__(self, max_num=1024):
+        self.n = max_num
+
+        self.A = ti.Vector.field(4, dtype=ti.f32)
+        self.B = ti.Vector.field(4, dtype=ti.f32)
+        self.C = ti.Vector.field(4, dtype=ti.f32)
+        self.vertices = ti.root.dense(ti.i, self.n).place(self.A, self.B, self.C)
+        self.triangle_num = ti.field(dtype=ti.i32, shape=())
+        self.color = ti.Vector.field(3, dtype=ti.f32, shape=max_num)
+
+    def reset(self):
+        self.triangle_num[None] = 0
+
+    def add_triangle(self, p1, p2, p3, color=None):
         if color is None:
             color = [1.0, 1.0, 1.0]
-        self.vertex = ti.Vector.field(4, dtype=ti.f32, shape=3)
-        self.color = ti.Vector.field(3, dtype=ti.f32, shape=())
-        self.color[None] = color
+        i = self.triangle_num[None]
+        self.A[i] = [p1.x, p1.y, p1.z, 1.0]
+        self.B[i] = [p2.x, p2.y, p2.z, 1.0]
+        self.C[i] = [p3.x, p3.y, p3.z, 1.0]
+        self.color[i] = color
+        self.triangle_num[None] += 1
 
-    def set_vertex(self, i, point):
-        self.vertex[i] = point / point[3]
-
+    @staticmethod
     @ti.func
-    def inside(self, pos):
+    def inside(pos, pa, pb, pc):
         is_inside = False
-        pa = ti.Vector([self.vertex[0].x, self.vertex[0].y])
-        pb = ti.Vector([self.vertex[1].x, self.vertex[1].y])
-        pc = ti.Vector([self.vertex[2].x, self.vertex[2].y])
+        # pa = ti.Vector([pa.x, pa.y])
+        # pb = ti.Vector([pb.x, pb.y])
+        # pc = ti.Vector([pc.x, pc.y])
         a = (pb - pa).cross(pos - pa)
         b = (pc - pb).cross(pos - pb)
         c = (pa - pc).cross(pos - pc)
@@ -152,22 +191,24 @@ class Triangle:
         return is_inside
 
 
-p1 = ti.Vector([0.0, 0.0, -2.0, 1])
-p2 = ti.Vector([0.0, 0.0, 2.0, 1])
-p3 = ti.Vector([0.0, 2.0, 0.0, 1])
-tri = Triangle()
-tri.set_vertex(0, p1)
-tri.set_vertex(1, p2)
-tri.set_vertex(2, p3)
+p0 = ti.Vector([1.0, 1.0, 1.0, 1])
+p1 = ti.Vector([-1.0, 1.0, 1.0, 1])
+p2 = ti.Vector([-1.0, -1.0, 1.0, 1])
+p3 = ti.Vector([1.0, -1.0, 1.0, 1])
+p4 = ti.Vector([1.0, 1.0, -1.0, 1])
+p5 = ti.Vector([-1.0, 1.0, -1.0, 1])
+p6 = ti.Vector([-1.0, -1.0, -1.0, 1])
+p7 = ti.Vector([1.0, -1.0, -1.0, 1])
 
-point = ti.Vector([1.1, 5.0, 5.0, 0.0])
-tri2 = Triangle(color=[0.0, 1.0, 0.5])
-tri2.set_vertex(0, p1 + point)
-tri2.set_vertex(1, p2 + point)
-tri2.set_vertex(2, p3 + point)
+triangles = Triangles()
+triangles.add_triangle(p0, p1, p2, RED)
+triangles.add_triangle(p2, p0, p3, RED)
+triangles.add_triangle(p2, p3, p6, GREEN)
+triangles.add_triangle(p3, p6, p7, GREEN)
+triangles.add_triangle(p4, p3, p0, BLUE)
+triangles.add_triangle(p4, p7, p3, BLUE)
 
 rst = Rasterizer()
-rst.add_triangle(tri)
-rst.add_triangle(tri2)
+rst.set_triangles(triangles)
 
 rst.show()
